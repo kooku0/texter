713c553480f547c9bb3c17e49091853ae9abcb91
kooku <kooku@kakao.com>
2022-02-27 11:36:44 +0900

    [Feature] 불필요한 페이지와 컴포넌트 제거 (#7)

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -2,14 +2,11 @@ import { Platform, StyleSheet , Text, View } from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Modal</Text>
       <View style={styles.separator} />
-      <EditScreenInfo path="/screens/ModalScreen.tsx" />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
