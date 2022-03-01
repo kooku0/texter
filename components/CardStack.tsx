d20173c77fcfe7778c45e44895ed73a29d65b19e
kooku <kooku@kakao.com>
2022-03-01 12:47:13 +0900

    feature: 외부라이브러리를 커스텀컴포넌트로 분리

--- components/CardStack.tsx
+++ components/CardStack.tsx
@@ -1,44 +1,23 @@
-
-
-
 import Swiper from 'react-native-deck-swiper';
 
-import React, { useRef } from 'react';
+import React, { ForwardedRef, forwardRef, ReactElement } from 'react';
 
-import Card from './Card';
+interface Props<CardProps> {
+  cards: CardProps[];
+  renderCard: (props: CardProps) => ReactElement;
+}
 
-function CardStack2() {
-  const swiperRef = useRef<Swiper<any>>(null);
-  const items = [
-    {
-      text: 'text1',
-      background: '#C0E9DC',
-    },
-    {
-      text: 'text2',
-      background: 'rgb(252, 188, 24)',
-    },
-    {
-      text: 'text3',
-      background: '#FFE4E4',
-    },
-  ];
-
-  const swipeLeft = () => {
-    if (swiperRef.current) {
-      swiperRef.current.swipeLeft();
-    }
-  };
+function CardStack<CardProps>(props: Props<CardProps>, ref: ForwardedRef<Swiper<CardProps>>) {
+  const { cards, renderCard: Card } = props;
 
   return (
     <Swiper
-      ref={swiperRef}
-      onTapCard={swipeLeft}
-      cards={items}
+      ref={ref}
+      cards={cards}
       cardIndex={0}
       cardVerticalMargin={30}
       cardHorizontalMargin={20}
-      renderCard={(props) => Card(props)}
+      renderCard={(cardProps) => Card(cardProps)}
       stackSize={3}
       stackSeparation={15}
       backgroundColor="white"
@@ -52,5 +31,5 @@ function CardStack2() {
   );
 }
 
-export default CardStack2;
+export default forwardRef<any, any>(CardStack);
 
